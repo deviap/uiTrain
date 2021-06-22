ed8167e960d59654006e99059eabc6bedb5e0871
utrain <skassem05@outlook.com>
2021-06-22 16:38:31 -0400

    Updated maid to be nicer. Changing it's api.

--- client/scripts/maid.lua
+++ client/scripts/maid.lua
@@ -1,154 +1,78 @@
--- By utrain
---[[
-	@Description
-		This object is helpeful in cleaning up mess. Consider it as a finalizer.
-	@Parameters
-		[nil]
-	@Returns
-		[table] maid
-	@Interface
-		[function] giveCallback([function] callback)
-		[number, function] giveEvent([number] eventId)
-		[table, function] giveObject([table] object)
-		[nil] cleanUp()
-		[nil] destroy()
-			Alias of cleanUp
-		[nil] cleanUpTarget([function] target)
---]]
-local ADD_CALLBACK_TYPE_ERROR =
-	"Argument #2's type is %s, expected function instead."
-local ADD_EVENT_TYPE_ERROR =
-	"Argument #2's type is %s, expected integer instead."
-local ADD_OBJECT_TYPE_ERROR =
-	"Argument #2's type is %s, expected table or tevObject instead."
-local ADD_OBJECT_MISSING_DESTROY_ERROR =
-	"Argument #2 is missing 'destroy' method."
 
-local cleanTarget = function(target)
+local INVALID_TASK_ADDED_ERROR = "Task given is the invalid type %s."
+local INVALID_ID_ERROR = "There's no task with the id of %s."
+local INVALID_TASK_CLEANED_ERROR = 
+	"The task with an id of %s has the invalid type of %s"
+
+local VALID_TYPES = {
+	tevObj = true,
+	table = true,
+	number = true,
+	["function"] = true,
+}
+
+local addTask = function(self, task, id)
+	if not VALID_TYPES[type(task)] then 
+		error(INVALID_TASK_ADDED_ERROR:format(type(task))) 
+	end
+	
+	id = id or #self._tasks + 1
+
+	if self._tasks[id] then
+		self:cleanTask(id)
+	end
+
+	self._tasks[id] = task
+
+	return id
+end
+
+local cleanTask = function(self, id)
+	local target = self._tasks[id] 
+		or error(INVALID_ID_ERROR:format(tostring(id)))
 	local targetType = type(target)
 
-	if targetType == "table" then
+	if targetType == "devObj" then
 		target:destroy()
-	elseif targetType == "tevObj" then
+	elseif targetType == "table" then
 		target:destroy()
-	elseif targetType == "number" then
-		core.disconnect(target)
 	elseif targetType == "function" then
 		target()
-	end
-end
-
-local giveCallback = function(self, callback)
-	--[[
-		@Description
-			Adds a function to be invoked whenever maid cleans up.
-		@Parameters
-			[table] maid
-			[function] callback()
-		@Returns
-			[function] callback
-	--]]
-
-	if type(callback) ~= "function" then
-		error(ADD_CALLBACK_TYPE_ERROR:format(type(callback)), 2)
+	elseif targetType == "number" then
+		core.disconnect(target)
+	else
+		error(INVALID_TASK_CLEANED_ERROR:format(tostring(id), targetType))
 	end
 
-	self._tasks[callback] = true
-	return callback
+	self._tasks[id] = nil
 end
 
-local giveEvent = function(self, eventId)
-	--[[
-		@Description
-			Pass an eventId to be disconnected when maid cleans up.
-		@Parameters
-			[table] maid
-			[number] eventId
-		@Returns
-			[number] eventId
-			[function] callback
-				This is the callback that will be invoked to disconnect the
-				event. Techincally, this is what is being added to the maid.
-	--]]
-
-	if type(eventId) ~= "number" then
-		error(ADD_EVENT_TYPE_ERROR:format(type(eventId)), 2)
-	end
-
-	self._tasks[eventId] = true
-	return eventId
-end
-
-local giveObject = function(self, object)
-	--[[
-		@Description
-			Pass an table to be cleaned up when maid cleans up. Requires that
-			it has a :destroy() method.
-		@Parameters
-			[table] maid
-			[table] object
-		@Returns
-			[table] object
-			[function] callback
-				This is the callback that will be invoked to clean the object. 
-				Techincally, this is what is being added to the maid.
-	--]]
-
-	if type(object) ~= "tevObj" then
-		if type(object) ~= "table" then
-			error(ADD_OBJECT_TYPE_ERROR:format(type(object)), 2)
+local cleanAll = function(self)
+	-- Events first, as they're sure to be cleaned up. And, other tasks may get
+	-- to them first without us knowing.
+	for id, task in next, self._tasks do
+		if type(task) == "number" then
+			core.disconnect(task)
+			self._tasks[id] = nil
 		end
 	end
 
-	if not object.destroy then error(ADD_OBJECT_MISSING_DESTROY_ERROR, 2) end
+	-- Cleaning up tasks may add more tasks to this maid, so we want to get
+	-- get them as well.
+	local id, _ = next(self._tasks, nil)
 
-	self._tasks[object] = true
-	return object
-end
-
-local cleanUp = function(self)
-	--[[
-		@Description
-			Cleans up the queued functions in maid asynchronously.
-		@Parameters
-			[table] maid
-		@Returns
-			[nil]
-	--]]
-
-	for target, _ in next, self._tasks do
-		cleanTarget(target)
-		self._tasks[target] = nil
-	end
-end
-
-local cleanUpTarget = function(self, target)
-	--[[
-		@Description
-			Cleans up the targeted function in maid.
-		@Parameters
-			[table] maid
-			[function] target
-		@Returns
-			[nil]	
-	--]]
-
-	for possibleTarget, _ in next, self._tasks do
-		if possibleTarget == target then
-			cleanTarget(target)
-			self._tasks[target] = nil
-		end
+	while id do
+		self:cleanUp(id)
+		id, _ = next(self._tasks, id)
 	end
 end
 
 return function()
 	return {
 		_tasks = {},
-		giveCallback = giveCallback,
-		giveEvent = giveEvent,
-		giveObject = giveObject,
-		cleanUp = cleanUp,
-		destroy = cleanUp, -- Alias
-		cleanUpTarget = cleanUpTarget
+		addTask = addTask,
+		cleanTask = cleanTask,
+		destroy = cleanAll,
+		cleanAll = cleanAll,
 	}
 end
